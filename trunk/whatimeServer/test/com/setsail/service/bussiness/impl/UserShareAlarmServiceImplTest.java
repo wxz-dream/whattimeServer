67c73ec646aa3cedc47a0ffbc07167dea495ade7
devilangel <devilangel@83d70b51-9ae3-4d13-af25-8470fbc94ee1>
2014-03-27 09:11:58 +0000

    转包

--- trunk/whatimeServer/test/com/setsail/service/bussiness/impl/UserShareAlarmServiceImplTest.java
+++ trunk/whatimeServer/test/com/setsail/service/bussiness/impl/UserShareAlarmServiceImplTest.java
@@ -3,16 +3,16 @@ package com.setsail.service.bussiness.impl;
 
 /*@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"/wtserver-servlet.xml","/repository.xml"})*/
-public class UserShareAlrmServiceImplTest {
+public class UserShareAlarmServiceImplTest {
 	/*
 	
 	@Resource
-	private UserShareAlrmService userShareAlrmService;
+	private UserShareAlarmService userShareAlarmService;
 	@Resource
 	private UserShareAlarmRepository userShareAlarmRepository;
 
 	@Test
-	public void testAlrmShareAddOrUpdate() {
+	public void testAlarmShareAddOrUpdate() {
 		System.out.println("=========");
 		String alarm = "{\"alarmTime\":1395502800952,\"createTime\":1395502644604,\"des\":\"vuvuvubu\",\"endTime\":1396107420132,\"froms\":1,\"id\":2,\"isDel\":false,\"isEndJoin\":false,\"open\":true,\"maxJoinNum\":10000,\"share\":\"0\",\"taskId\":2,\"taskUuid\":\"d21e4012-df9e-496f-bf52-f198ff32adf9\",\"tasks\":[{\"address\":\"uguvuv&\",\"alarmId\":2,\"alarmTime\":1395502800952,\"alarmUuid\":\"3bfd19ca-5bc8-48d9-a737-d3095a101504\",\"delayMinute\":1,\"delayOrder\":1,\"delayType\":1,\"des\":\"fyfufuf7\",\"id\":2,\"isDel\":false,\"isOpen\":true,\"music\":\"content://settings/system/alarm_alert\",\"notice\":\"jvjvjvuv\",\"playType\":0,\"repeatType\":1,\"shake\":true,\"title\":\"yufyfyw\",\"uuid\":\"d21e4012-df9e-496f-bf52-f198ff32adf9\"}],\"title\":\"yvyvyv\",\"type\":0,\"uptTime\":1395502690938,\"uuid\":\"3bfd19ca-5bc8-48d9-a737-d3095a101504\"}";
 		UserShareAlarm  userShare = JSONObject.parseObject(alarm, UserShareAlarm.class);
@@ -24,12 +24,12 @@ public class UserShareAlrmServiceImplTest {
 	}
 
 //	@Test
-	public void testAlrmShareDelete() {
+	public void testAlarmShareDelete() {
 //		fail("Not yet implemented");
 		userShareAlarmRepository.delete("3bfd19ca-5bc8-48d9-a737-d3095a101504");
 	}
 
-	public void testAlrmShareGetLastSyncTime() {
+	public void testAlarmShareGetLastSyncTime() {
 		fail("Not yet implemented");
 	}
 	
