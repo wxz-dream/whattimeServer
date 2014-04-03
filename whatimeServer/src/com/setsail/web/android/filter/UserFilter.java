cfa1a7527fea76dcd66ff7c9ce7374507e8daf00
wadezyj <wadezyj@83d70b51-9ae3-4d13-af25-8470fbc94ee1>
2014-04-03 15:48:06 +0000

    登陆拦截器改进，不需要再过滤器在添加控制器类名

--- whatimeServer/src/com/setsail/web/android/filter/UserFilter.java
+++ whatimeServer/src/com/setsail/web/android/filter/UserFilter.java
@@ -11,6 +11,9 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -33,19 +36,19 @@ import com.setsail.entity.response.StateEnum;
 import com.setsail.entity.response.SystemState;
 import com.setsail.entity.user.User;
 import com.setsail.repository.user.UserRepository;
+import com.setsail.util.PackageScan;
 import com.setsail.util.SstringUtils;
 import com.setsail.web.android.annotation.Auth;
 
 /**
  * 
- * @WebFilter将一个实现了javax.servlet.Filte接口的类定义为过滤器组件 属性filterName声明过滤器的名称,可选
- *                                                 属性urlPatterns指定要过滤
- *                                                 的URL模式,也可使用属性value来声明
- *                                                 .(指定要过滤的URL模式是必选属性)
+ * @WebFilter将一个实现了javax.servlet.Filte接口的类定义为过滤器组件 
+ * 属性filterName声明过滤器的名称,可选属性urlPatterns指定要过滤
+ * 的URL模式,也可使用属性value来声明.(指定要过滤的URL模式是必选属性)
  * 
- *                                                 <p>
- *                                                 功能描述: 过滤器
- *                                                 </p>
+ * <p>
+ * 功能描述: 过滤器
+ * </p>
  * @author: zhangyajie
  * @version 0.1
  */
@@ -54,13 +57,7 @@ public class UserFilter implements Filter {
 
 	// private Logger logger = Logger.getLogger(getClass());
 	private String[] noLoginAuthURL = {};
-	private String[] controllerPackage = new String[] {
-			"com.setsail.web.android.controller.user.UserController",
-			"com.setsail.web.android.controller.business.UserShareAlarmController",
-			"com.setsail.web.android.controller.business.UserLocalAlarmController",
-			"com.setsail.web.android.controller.business.CategoryController",
-			"com.setsail.web.android.controller.business.MarketController",
-			};
+	private String basePackage = "com.setsail.web.android.controller";
 	private String MIME = "MIME";
 	private String UUID = "userUuid";
 
@@ -118,6 +115,7 @@ public class UserFilter implements Filter {
 	 */
 	private boolean contains(String[] nologinAuthURL, String url) {
 		if (nologinAuthURL.length > 0) {
+			if(url.contains(".")) url = url.substring(0, url.lastIndexOf("."));
 			for (String str : nologinAuthURL) {
 				if (str.equalsIgnoreCase(url)) {
 					return true;
@@ -129,78 +127,105 @@ public class UserFilter implements Filter {
 
 	@Override
 	public void init(FilterConfig fc) throws ServletException {
-		noLoginAuthURL = getNoLoginURL(controllerPackage);
-		System.out.println("登陆拦截的链接初始化完毕" + Arrays.toString(noLoginAuthURL));
-		controllerPackage = null;
+		noLoginAuthURL = getNoLoginURL(basePackage);
+		System.out.println("login filter init success : " + Arrays.toString(noLoginAuthURL));
+		basePackage = null;
 		// logger.info("登陆拦截的链接初始化完毕:" + Arrays.toString(noLoginAuthURL));
 	}
-
-	private String[] getNoLoginURL(String[] classNames) {
-		if (classNames != null) {
-
-			ArrayList<String> urList = new ArrayList<String>();
-
-			for (String className : classNames) {
-				@SuppressWarnings("rawtypes")
-				Class controller = null;
-
-				try {
-					controller = Class.forName(className);
-				} catch (SecurityException e) {
-					e.printStackTrace();
-					// logger.error(e);
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-					// logger.error(e);
+	
+	private String[] getNoLoginURL(String basePackage){
+		List<String> urls = new ArrayList<String>(50);
+		Set<Class<?>> sets = PackageScan.getClasses(basePackage);
+		Iterator<Class<?>> it;
+		if(sets != null && (it = sets.iterator()) != null){
+			while(it.hasNext()){
+				Class<?> controllerClass = it.next();
+				Controller controller = AnnotationUtils.findAnnotation(controllerClass, Controller.class);
+				if(controller == null) continue;
+				Auth auth = AnnotationUtils.findAnnotation(controllerClass, Auth.class);
+				if(auth == null) {
+					urls.addAll(getNotLoginURLByMethod(controllerClass));
 				}
-				if (controller != null) {
+				else {
+					urls.addAll(getNotLoginURLByClass(controllerClass));
+				}
+			}
+		}
+		return urls.toArray(noLoginAuthURL);
+	}
 
-					Controller c = AnnotationUtils.findAnnotation(controller,
-							Controller.class);
+	private List<String> getNotLoginURLByMethod(Class<?> controllerClass) {
+		List<String> urList = new ArrayList<String>(10);
+		RequestMapping rmc = AnnotationUtils.findAnnotation(controllerClass, RequestMapping.class);
+		if(rmc == null) return urList;
+		String[] prefixs = rmc.value();
+		if(hasLength(prefixs)) return urList;
+		Method[] ms = controllerClass.getMethods();
+		if(hasLength(ms)) return urList;
+		for (Method m : ms) {
+			Auth auth = AnnotationUtils.findAnnotation(m,
+					Auth.class);
+			// 若有Auth注解，则该方法不需要登陆验证
+			if (auth != null) {
+				RequestMapping rm = AnnotationUtils
+						.findAnnotation(m,
+								RequestMapping.class);
+				if (rm != null) {
+					String[] suffixs = rm.value();
+					if (suffixs != null) {
+						for (String suffix : suffixs) {
 
-					if (c != null) {
-						// controller类RequestMapping注解
-						RequestMapping crm = AnnotationUtils.findAnnotation(
-								controller, RequestMapping.class);
-						// url前缀
-						String[] prefixs = crm == null ? new String[0] : crm
-								.value();
-
-						Method[] ms = controller.getMethods();
-						if (ms != null) {
-							for (Method m : ms) {
-								Auth auth = AnnotationUtils.findAnnotation(m,
-										Auth.class);
-								// 若有Auth注解，则该方法不需要登陆验证
-								if (auth != null) {
-									RequestMapping rm = AnnotationUtils
-											.findAnnotation(m,
-													RequestMapping.class);
-									if (rm != null) {
-										String[] suffixs = rm.value();
-										if (suffixs != null) {
-											for (String suffix : suffixs) {
-
-												if (prefixs != null
-														&& prefixs.length > 0)
-													for (String prefix : prefixs) {
-														urList.add(prefix
-																+ suffix);
-													}
-												else {
-													urList.add(suffix);
-												}
-											}
-										}
-									}
+							if (prefixs != null
+									&& prefixs.length > 0)
+								for (String prefix : prefixs) {
+									urList.add(prefix
+											+ suffix);
 								}
+							else {
+								urList.add(suffix);
 							}
 						}
 					}
 				}
 			}
-			return urList.toArray(noLoginAuthURL);
 		}
-		return new String[0];
+		return urList;
 	}
+
+	private List<String> getNotLoginURLByClass(
+			Class<?> controllerClass) {
+		List<String> urList = new ArrayList<String>(10);
+		RequestMapping rmc = AnnotationUtils.findAnnotation(controllerClass, RequestMapping.class);
+		if(rmc == null) return urList;
+		String[] prefixs = rmc.value();
+		if(hasLength(prefixs)) return urList;
+		Method[] ms = controllerClass.getMethods();
+		if(hasLength(ms)) return urList;
+		for (Method m : ms) {
+			RequestMapping rm = AnnotationUtils.findAnnotation(m, RequestMapping.class);
+			if (rm != null) {
+				String[] suffixs = rm.value();
+				if (suffixs != null) {
+					for (String suffix : suffixs) {
+
+						if (prefixs != null
+								&& prefixs.length > 0)
+							for (String prefix : prefixs) {
+								urList.add(prefix
+										+ suffix);
+							}
+						else {
+							urList.add(suffix);
+						}
+					}
+				}
+			}
+		}
+		return urList;
+	}
+	
+	private boolean hasLength(Object[] array){
+		return array == null || array.length == 0;
+	}
+	
 }
